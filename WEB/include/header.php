45ba8689d6d8b3aad171793ba8f81f3df5ba59da
lukacetina <luka.cetina@student.um.si>
2018-11-25 20:34:39 +0100

    Naredil prijavo in odjavo

---
+++ WEB/include/header.php
@@ -0,0 +1,23 @@
+<header class="mdc-top-app-bar">
+    <div class="mdc-top-app-bar__row">
+        <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+            <!--<a href="#" class="material-icons mdc-top-app-bar__navigation-icon">menu</a>-->
+            <a href="index.php"><span class="mdc-top-app-bar__title">Kino Študent</span></a>
+        </section>
+        <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
+            <?php
+            if(@isset($_SESSION["uporabnik"])){?>
+                <a onclick="odjava()"><div class="navbarLink  waves-effect waves-grey-lighten-5">Odjava</div></a>
+                <a ><div class="navbarLink  waves-effect waves-grey-lighten-5">Moj račun</div></a>
+            <?php
+            }
+            else{?>
+                <a onclick="naloziPrijavo()"><div class="navbarLink  waves-effect waves-grey-lighten-5">Prijava</div></a>
+                <a onclick="naloziRegistracijo()"><div class="navbarLink  waves-effect waves-grey-lighten-5">Registracija</div></a>
+            <?php
+            }
+            ?>
+            <a ><div class="navbarLink waves-effect waves-grey-lighten-5">Košarica</div></a>
+        </section>
+    </div>
+</header>
--- WEB/index.php
+++ WEB/index.php
@@ -1,3 +1,53 @@
 <?php
-    echo "test";
+    session_start();
+    if(@isset($_POST["ime"]) && @!isset($_SESSION["uporabnik"])){
+        if(($_POST["ime"]=="Luka" && $_POST["geslo"]=="123")||($_POST["ime"]=="David" && $_POST["geslo"]=="123")){
+            $_SESSION["uporabnik"] = $_POST["ime"];
+            $_SESSION["prijavljen"] = true;
+        }
+    }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <?php
+        include "include/import.html";
+    ?>
+    <title>Kino IS</title>
+</head>
+<body>
+<?php
+    include "include/header.php";
+?>
+<div id="content">
+    <?php
+        if(@$_SESSION["prijavljen"]==true)
+            if(@$_SESSION["uporabnik"]=="Luka")
+                echo @$_SESSION["uporabnik"]." je zelo kul oseba ";
+            elseif(@$_SESSION["uporabnik"]=="David")
+                echo "O, ".@$_SESSION["uporabnik"].", a ste kej slabi?";
+        elseif(@isset($_POST["ime"])){
+            echo "prijava ni uspela";
+        }
+    ?>
+</div>
+<script>
+    function naloziPrijavo(){
+        $("#content").load("prijava.html");
+    }
+
+    function odjava(){
+        //$("html").load("index.html?odjavi=true");
+        $.ajax({url: "index.php?odjavi=true", success: function(result){
+            $("body").html(result);
+        }});
+    }
+    function naloziRegistracijo(){
+        $("#content").load("registracija.html");
+    }
+    mdc.ripple.MDCRipple.attachTo(document.querySelector('button'));
+    mdc.ripple.MDCRipple.attachTo(document.querySelector('a'));
+</script>
+</body>
+</html>
